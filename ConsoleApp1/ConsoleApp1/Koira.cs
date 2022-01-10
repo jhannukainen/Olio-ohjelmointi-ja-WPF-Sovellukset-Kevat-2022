64b02177fa182c144e49698c8be8fac481294d85
Jonne Hannukainen <jonne.hannukainen@student.careeria.fi>
2022-01-10 13:00:44 +0200

    Koira esimerkki

---
+++ ConsoleApp1/ConsoleApp1/Koira.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Koira
+       
+    {
+        public string nimi;
+        public int ikä;
+        public string rotu;
+
+        public void TulostaData()
+        {
+            Console.WriteLine("Koiran nimi: " + nimi + ", ikä: " + ikä + ", Rotu:" + rotu);
+
+
+        }
+
+        public void Hauku()
+        {
+            Console.WriteLine("Woof Woof");
+        }
+    
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Koira koira1 = new Koira();
+            koira1.nimi = "Rekku";
+            koira1.ikä = 4;
+            koira1.rotu = "Saksanpaimenkoira";
+
+            koira1.Hauku();
+            koira1.TulostaData();
+
+            Koira koira2 = new Koira();
+            koira2.nimi = "Musti";
+            koira2.ikä = 2;
+            koira2.rotu = "Mopsi";
+
+            koira2.TulostaData();
+          
         }
     }
 }
